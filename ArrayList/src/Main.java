23d5dd6d8f83f245d5ecb245d6f74e99188a1f94
rinshad101 <rinshadkk1111@gmail.com>
2025-03-03 15:50:54 +0530

    new

--- ArrayList/src/Main.java
+++ ArrayList/src/Main.java
@@ -1,7 +1,22 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
 
+        List<String> name = new ArrayList<>();
+
+        name.add("rinshad");
+        name.add("aginas");
+        name.add("amir");
+        name.add("minhaj");
+
+        System.out.println(name);
+
+        name.remove(1);
+
+        System.out.println(name);
+
+        System.out.println(name.get(1));
     }
 }
