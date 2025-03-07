3cf983da39a1776085500147af9e2ebf57d37079
rinshad101 <rinshadkk1111@gmail.com>
2025-03-07 21:54:45 +0530

    new

---
+++ JDBC/src/Main.java
@@ -0,0 +1,21 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://localhost:3306/testdb";
+        String user = "Rinshad";
+        String password = "rinshad3657";
+
+        try {
+            Connection con = DriverManager.getConnection(url, user, password);
+            System.out.println("connected successfully");
+            con.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
