a6675aa4a925cbbd3632ecaba525091f3b61e437
rinshad101 <rinshadkk1111@gmail.com>
2025-03-09 13:28:28 +0530

    new

--- MultiThreading/src/Main.java
+++ MultiThreading/src/Main.java
@@ -1,12 +1,25 @@
-// Creating a Thread Using the Thread Class
-class MyTread extends Thread{
-    public void run(){
-        for (int i=1; i<10;i++){
+// Creating a Multiple Thread Using the Runnable Class
+class MyTread implements Runnable {
+    public void run() {
+        for (int i = 1; i < 10; i++) {
             System.out.println("thread -- " + i);
-            try{
-                Thread.sleep(1000);
-            } catch (InterruptedException e){
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
                 System.out.println("thread interupted..");
+            }
+        }
+    }
+}
+
+class MyThread1 implements Runnable {
+    public void run() {
+        for (int i = 1; i < 10; i++) {
+            System.out.println("threeeead  --- " + i);
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                System.out.println("  ... . .");
             }
         }
     }
@@ -14,10 +27,20 @@ class MyTread extends Thread{
 
 public class Main {
     public static void main(String[] args) {
-        MyTread trdObj = new MyTread();
-        trdObj.start();
-        System.out.println(trdObj.isAlive());
-        System.out.println(trdObj.getName());
-        System.out.println(Thread.activeCount());
+
+
+//        System.out.println(trdObj.isAlive());
+//        System.out.println(trdObj.getName());
+//        System.out.println(Thread.activeCount());
+
+        MyTread obj = new MyTread();
+        MyThread1 obj2 = new MyThread1();
+
+        Thread t1 = new Thread(obj);
+        Thread t2 = new Thread(obj2);
+
+        t1.start();
+        t2.start();
+
     }
 }
